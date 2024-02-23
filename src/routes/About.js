9f91f966374df494a76c3526262b7c59ac6b5188
Mon Cedric Avila <moncedricavila@gmail.com>
2024-02-22 20:04:38 -0500

    error

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+
 const About = () => {
   return (
     <div>
